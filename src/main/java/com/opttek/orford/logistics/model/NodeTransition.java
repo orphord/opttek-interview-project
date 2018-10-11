4647138ae5708388d5af86714723c9f6706034b1
Jeff Orford <orphord@gmail.com>
2018-10-11 15:07:58 -0600

    Added equals() and toString()

--- src/main/java/com/opttek/orford/logistics/model/NodeTransition.java
+++ src/main/java/com/opttek/orford/logistics/model/NodeTransition.java
@@ -57,4 +57,37 @@ public class NodeTransition implements Comparable<NodeTransition>{
 		this.transitionCost = _transitionCost;
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		// If the object is compared with itself then return true
+		if (o == this) {
+			return true;
+		}
+
+		/*
+		 * Check if o is an instance of Node or not "null instanceof [type]" also
+		 * returns false
+		 */
+		if (!(o instanceof NodeTransition)) {
+			return false;
+		}
+
+		// typecast o to Complex so that we can compare data members
+		NodeTransition c = (NodeTransition) o;
+
+		// Compare the data members and return accordingly
+		return this.getToNode().equals(c.getToNode()) && this.getTransitionCost().equals(c.getTransitionCost());
+
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer buf = new StringBuffer("NodeTransition {\n");
+		buf.append("\tFromNode: " + this.getFromNode().toString() + ",\n");
+		buf.append("\ttoNode: " + this.getToNode().toString() + ",\n");
+		buf.append("\ttransitionCost: " + this.getTransitionCost() + "\n");
+		buf.append("}\n");
+
+		return buf.toString();
+	}
 }
